c472e9fa6444afb74d112d8f76802c7e8f354646
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-06-22 16:13:35 -0600

    estructura basica

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes =[
+    new Recipe('A recipe', 'This is an exame', 'https://cdn.onemars.net/sites/nutro_es_NkyIN_B9cV/image/gettyimages-605382371_1615921436612.jpeg')
+  ];
 
   constructor() { }
 
