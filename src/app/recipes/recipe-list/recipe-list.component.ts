50fa3dbc032555c577fb505764aee5a15f636a53
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-06-23 12:47:13 -0600

    enviar elemtos con el Binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -9,16 +9,22 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes =[
-    new Recipe('A recipe', 'This is an example',
+    new Recipe('A recipe of Caviar', 'This is an example of Caviar',
      'https://cdn.onemars.net/sites/nutro_es_NkyIN_B9cV/image/gettyimages-605382371_1615921436612.jpeg'),
-     new Recipe('A recipe', 'This is an example',
+     new Recipe('A recipe of Lobster', 'This is an example of Lobster',
      'https://cdn.onemars.net/sites/nutro_es_NkyIN_B9cV/image/gettyimages-605382371_1615921436612.jpeg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
