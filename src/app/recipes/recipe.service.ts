f462f5eb498197f9c7401a584e305d3f18707187
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-06-28 18:02:26 -0600

    add ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,36 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes =[
-    new Recipe('A recipe of Caviar', 'This is an example of Caviar',
-     'https://cdn.onemars.net/sites/nutro_es_NkyIN_B9cV/image/gettyimages-605382371_1615921436612.jpeg'),
-     new Recipe('A recipe of Lobster', 'This is an example of Lobster',
-     'https://cdn.onemars.net/sites/nutro_es_NkyIN_B9cV/image/gettyimages-605382371_1615921436612.jpeg')
+    new Recipe('A recipe of Caviar',
+    'This is an example of Caviar',
+     'https://cdn.onemars.net/sites/nutro_es_NkyIN_B9cV/image/gettyimages-605382371_1615921436612.jpeg',
+     [
+       new ingredient('Meat', 1),
+       new ingredient('French Fries', 20),
+     ]),
+     new Recipe('A recipe of Lobster',
+     'This is an example of Lobster',
+     'https://cdn.onemars.net/sites/nutro_es_NkyIN_B9cV/image/gettyimages-605382371_1615921436612.jpeg',
+     [
+       new ingredient('Buns', 2),
+       new ingredient('Meat', 1),
+     ])
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes(){
     return this.recipes.slice();
+  }
+
+  addToShoppingList(ingredients: ingredient[]){
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
