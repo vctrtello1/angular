810fbb7ec7c9907c5dcdaee27a0c03c9a7b8bfaf
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-06 16:04:58 -0600

    catch errors in observable

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -30,8 +30,8 @@ export class AuthService{
       email: email,
       password: password,
       returnSecureToken: true
-    })
-    .pipe(catchError(this.handlerError));
+    }
+  ).pipe(catchError(this.handlerError));
   }
 
   login(email: string, password: string){
@@ -42,7 +42,7 @@ export class AuthService{
         password: password,
         returnSecureToken: true
       }
-      );
+    ).pipe(catchError(this.handlerError));
   }
 
   private handlerError(errorRes: HttpErrorResponse){
@@ -53,6 +53,22 @@ export class AuthService{
       switch(errorRes.error.error.message){
         case 'EMAIL_EXISTS':
           errorMessage = 'The email already exists.';
+          break;
+        case 'OPERATION_NOT_ALLOWED':
+          errorMessage = 'Password sign-in is disabled for this project.';
+          break;
+        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+          errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+          break;
+        case 'EMAIL_NOT_FOUND':
+          errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+          break;
+        case 'INVALID_PASSWORD':
+          errorMessage = 'The password is invalid or the user does not have a password.';
+          break;
+        case 'USER_DISABLED':
+          errorMessage = 'The user account has been disabled by an administrator.';
+          break;
       }
       return throwError(errorMessage);
 
