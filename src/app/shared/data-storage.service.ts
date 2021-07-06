a74006f164e5b5e60d5ee933b0468c5d907d3141
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-06 10:14:27 -0600

    resolver para fetch recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from 'rxjs/operators'
+import { map,tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +19,7 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://recipes-38079-default-rtdb.firebaseio.com/recipes.json')
+    return this.http.get<Recipe[]>('https://recipes-38079-default-rtdb.firebaseio.com/recipes.json')
     .pipe(map(recipes => {
       return recipes.map(recipe => {
         return {
@@ -27,10 +27,9 @@ export class DataStorageService {
           ingredients: recipe.ingredients ? recipe.ingredients: []
         };
       });
-    }))
-    .subscribe(recipes => {
+    }),
+    tap(recipes => {
       this.recipesService.setRecipes(recipes);
-      console.log(recipes);
-    });
+    }));
   }
 }
