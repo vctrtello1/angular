2f59fa00eae7f9215266de8d272a49689956505b
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-08 10:09:07 -0600

    revisar Auth con header.component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
@@ -6,14 +8,32 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy{
+  isAuthenticated = false;
+  private userSubscription: Subscription;
+
+  constructor(private dataStorageService: DataStorageService,
+    private authService: AuthService) {}
 
   onSaveData(){
     this.dataStorageService.storeRecipes();
   }
 
   onFetchData(){
-    this.dataStorageService.fetchRecipes().subscribe();
+    this.dataStorageService.fetchRecipes().subscribe(
+      user => {
+        this.isAuthenticated = !!user;
+        console.log(!user);
+        console.log(!!user);
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.userSubscription = this.authService.user.subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 }
