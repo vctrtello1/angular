04e8cb6d145359bc55e7f0e72fc63a2df9b6b06a
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-08 17:10:13 -0600

    autologout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject, throwError } from "rxjs";
 import { catchError, tap } from 'rxjs/operators'
 import { User } from "./user.model";
 
-export interface AuthResponseData{
+export interface AuthResponseData {
   idToken: string,
   email: string,
   refresh_token: string,
@@ -20,28 +20,29 @@ export interface AuthResponseData{
     providedIn: 'root'
   }
 )
-export class AuthService{
+export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
-  constructor(private http: HttpClient, private router: Router){}
+  constructor(private http: HttpClient, private router: Router) { }
 
-  signup(email: string, password: string){
+  signup(email: string, password: string) {
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCf3mVU1uV0zk2RrW5NyPgQHSQrHWRC7u4',
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }
-  ).pipe(catchError(this.handlerError),tap(
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(catchError(this.handlerError), tap(
       respData => {
-        this.handlerAuthentication(respData.email, respData.localId,respData.idToken,
-           +respData.expiresIn)
+        this.handlerAuthentication(respData.email, respData.localId, respData.idToken,
+          +respData.expiresIn)
       }
     ));
   }
 
-  login(email: string, password: string){
+  login(email: string, password: string) {
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCf3mVU1uV0zk2RrW5NyPgQHSQrHWRC7u4',
       {
@@ -49,75 +50,92 @@ export class AuthService{
         password: password,
         returnSecureToken: true
       }
-    ).pipe(catchError(this.handlerError),tap(
+    ).pipe(catchError(this.handlerError), tap(
       respData => {
-        this.handlerAuthentication(respData.email, respData.localId,respData.idToken,
-           +respData.expiresIn)
+        this.handlerAuthentication(respData.email, respData.localId, respData.idToken,
+          +respData.expiresIn)
       }
     ));
   }
 
-  private handlerError(errorRes: HttpErrorResponse){
+  private handlerError(errorRes: HttpErrorResponse) {
     let errorMessage = 'An unknown error occurred!';
-      if (!errorRes.error || !errorRes.error.error){
-        return throwError(errorMessage);
-      }
-      switch(errorRes.error.error.message){
-        case 'EMAIL_EXISTS':
-          errorMessage = 'The email already exists.';
-          break;
-        case 'OPERATION_NOT_ALLOWED':
-          errorMessage = 'Password sign-in is disabled for this project.';
-          break;
-        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-          errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
-          break;
-        case 'EMAIL_NOT_FOUND':
-          errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
-          break;
-        case 'INVALID_PASSWORD':
-          errorMessage = 'The password is invalid or the user does not have a password.';
-          break;
-        case 'USER_DISABLED':
-          errorMessage = 'The user account has been disabled by an administrator.';
-          break;
-      }
+    if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'The email already exists.';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign-in is disabled for this project.';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password.';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'The user account has been disabled by an administrator.';
+        break;
+    }
+    return throwError(errorMessage);
 
   }
 
-  private handlerAuthentication(email: string, userId: string, token: string, expiresIn: number){
-    const expirationDate = new Date().getTime() + expiresIn * 10000;
-        const user = new User(
-          email, userId,
-          token, expirationDate
-          );
-          this.user.next(user);
-          localStorage.setItem('userData', JSON.stringify(user));
+  private handlerAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(
+      email, userId,
+      token, expirationDate
+    );
+    this.user.next(user);
+    this.autologout(expiresIn * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
-  logout(){
+  logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
   }
 
-  autologin(){
+  autologin() {
     const userData: {
       email: string, id: string,
       _token: string, _tokenExpirationDate: string
     } = JSON.parse(localStorage.getItem('userData'));
-    if(!userData){
+    if (!userData) {
       return;
     }
 
     const loadedUser = new User(
       userData.email, userData.id, userData._token,
       new Date(userData._tokenExpirationDate)
-      );
+    );
 
-      if (loadedUser.token) {
-        this.user.next(loadedUser);
-      }
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autologout(expirationDuration);
+    }
+  }
+
+  autologout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
 }
