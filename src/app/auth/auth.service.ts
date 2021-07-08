9a738e380837231bb6a3650983b99a4f55936bd8
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-08 14:00:43 -0600

    autologin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -93,11 +93,31 @@ export class AuthService{
           token, expirationDate
           );
           this.user.next(user);
+          localStorage.setItem('userData', JSON.stringify(user));
   }
 
   logout(){
     this.user.next(null);
     this.router.navigate(['/auth']);
+  }
+
+  autologin(){
+    const userData: {
+      email: string, id: string,
+      _token: string, _tokenExpirationDate: string
+    } = JSON.parse(localStorage.getItem('userData'));
+    if(!userData){
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email, userData.id, userData._token,
+      new Date(userData._tokenExpirationDate)
+      );
+
+      if (loadedUser.token) {
+        this.user.next(loadedUser);
+      }
   }
 
 }
