21d839be71a7b7e6702016d186d743413c16fa81
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-13 13:39:17 -0600

    auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppigListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,7 +26,8 @@ import { CoreModule } from './core.module';
     RecipesModule,
     ShoppigListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
