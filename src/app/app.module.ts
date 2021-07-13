20ef576a1f03614bda3f41b728d91867fd7b10b6
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-13 13:08:10 -0600

    shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,20 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { DropdownDirective } from './shared/dropdown.directive'
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppigListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
     AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -34,7 +27,8 @@ import { ShoppigListModule } from './shopping-list/shopping-list.module';
     ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
-    ShoppigListModule
+    ShoppigListModule,
+    SharedModule
   ],
   providers: [ShoppingListService, RecipeService,
   {
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { shoppingListRoutingModule } from "./shopping-list-routing.module";
 import { ShoppingListComponent } from "./shopping-list.component";
@@ -13,7 +13,7 @@ import { ShoppingListComponent } from "./shopping-list.component";
   ],
   imports: [
     RouterModule,
-    CommonModule,
+    SharedModule,
     FormsModule,
     shoppingListRoutingModule
   ]
