39b7dacd2bb499511fa10b7640805ae9d5d67e8f
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-13 12:47:12 -0600

    shopping-list module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -25,14 +25,6 @@ import { RecipesComponent } from "./recipes.component";
     CommonModule,
     ReactiveFormsModule,
     RecipesRoutingModule
-  ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
   ]
 })
 export class RecipesModule {
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { shoppingListRoutingModule } from "./shopping-list-routing.module";
+import { ShoppingListComponent } from "./shopping-list.component";
+
+@NgModule( {
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
+  imports: [
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    shoppingListRoutingModule
+  ]
+}
+)
+
+export class ShoppigListModule {
+
+}
